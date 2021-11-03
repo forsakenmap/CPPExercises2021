45b1595f950b2fd0284e628ec325bf7c14b8300e
forsakenmap <57358786+forsakenmap@users.noreply.github.com>
2021-11-03 11:08:19 +0300

    nmlk
    lk;k'

--- lesson06/src/blur.cpp
+++ lesson06/src/blur.cpp
@@ -1,63 +1,33 @@
 #include "blur.h"
+#define _USE_MATH_DEFINES
 #include <math.h>
-#include <libutils/rasserts.h>
-double pi = 3.14159265359;
-cv::Mat blur(cv::Mat img, double sigma) {
-    // вычислим среднеквадратичное отклонение, пусть сигма это размер матрицы +- от центрального пикселя (квадрат берём 3*3 5*5 итд)
-    // среднее арифметическое
-        cv::Mat img1 = img.clone();
-    for(int x = sigma-1; x < img.cols-sigma+1; x++) {
-        for(int y = sigma-1; y < img.rows-sigma+1; y++) {
-            double sumr, colr;
-            colr = 0;
-            sumr = 0;
-            double sumg, colg;
-            colg = 0;
-            sumg = 0;
-            double sumb, colb;
-            colb = 0;
-            sumb = 0;
-            for(int i = x - sigma; i <= x + sigma; i++) {
-                for(int j = y - sigma; j <= y + sigma; j++) {
-                    cv::Vec3b c = img.at<cv::Vec3b>(i, j);
-                    int r = c[2];
-                    colr++;
-                    sumr += r;
-                    int g = c[1];
-                    colg++;
-                    sumg += g;
-                    int b = c[0];
-                    colb++;
-                    sumb += b;
+
+cv::Mat blur(cv::Mat img, double s, int r) {
+    cv::Mat ks(2*r + 1, 2*r + 1, CV_64F);
+    for(int di = 0; di < ks.rows; di++)
+        for(int dj = 0; dj < ks.cols; dj++)
+            ks.at<double>(di, dj) = G(di-r, dj-r, s);
+    cv::Mat res = img.clone();
+    for(int i = 0; i < res.rows; i++) {
+        for (int j = 0; j < res.cols; j++) {
+            cv::Vec3d value(0,0,0);
+            double k1 = 0;
+            for(int di = -r; di <= r; di++)
+                for(int dj = -r; dj <= r; dj++){
+                    if(i+di < 0 || i+di >= res.rows || j+dj < 0 || j+dj >= res.cols)
+                        continue;
+                    double k = ks.at<double>(di+r, dj+r);
+                    cv::Vec3b c = img.at<cv::Vec3b>(i+di, j+dj);
+                    value += c*k;
+                    k1 += k;
                 }
-            }
-            sumb /= colb;
-            sumr /= colr;
-            sumg /= colg;
-            double kvg = 0;
-            double kvb = 0;
-            double kvr = 0;
-            for(int i = x - sigma; i <= x + sigma; i++) {
-                for(int j = y - sigma; j <= y + sigma; j++) {
-                    cv::Vec3b c = img.at<cv::Vec3b>(i, j);
-                    int r = c[2];
-                    kvr += (sumr - r)*(sumr - r);
-                    int g = c[1];
-                    kvg += (sumg - g)*(sumg - g);
-                    int b = c[0];
-                    kvb += (sumb - b)*(sumb - b);
-                }
-            }
-            kvr /= colr;
-            kvg /= colg;
-            kvb /= colb;
-            unsigned int r = (1/(2*pi*kvr*kvr))*exp(-(x*x+y*y)/(2*kvr*kvr));
-            unsigned int g = (1/(2*pi*kvg*kvg))*exp(-(x*x+y*y)/(2*kvg*kvg));
-           unsigned int b = (1/(2*pi*kvb*kvb))*exp(-(x*x+y*y)/(2*kvb*kvb));
-            img1.at<cv::Vec3b>(x, y) = cv::Vec3b(b,g,r);
+            value /= k1;
+            res.at<cv::Vec3b>(i, j) = value;
         }
     }
-    // TODO формулу весов можно найти тут:
-    // https://ru.wikipedia.org/wiki/%D0%A0%D0%B0%D0%B7%D0%BC%D1%8B%D1%82%D0%B8%D0%B5_%D0%BF%D0%BE_%D0%93%D0%B0%D1%83%D1%81%D1%81%D1%83
-    return img1;
+    return res;
+}
+
+double G(int dx, int dy, double s){
+    return exp(-(dx*dx+dy*dy)/(2*s*s))/(2*M_PI*s*s);
 }
